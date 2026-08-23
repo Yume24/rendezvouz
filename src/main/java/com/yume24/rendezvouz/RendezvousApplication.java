2d7dfe46935098f667ba49d612b3fed4b47f0cba
Antoni Jaros <antonijaros04@gmail.com>
2026-08-23 19:20:36 +0200

    fix: correct class and test names from `Rendevouz` to `Rendezvous`

--- src/main/java/com/yume24/rendezvouz/RendezvousApplication.java
+++ src/main/java/com/yume24/rendezvouz/RendezvousApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RendevouzApplication {
+public class RendezvousApplication {
 
     static void main(String[] args) {
-        SpringApplication.run(RendevouzApplication.class, args);
+        SpringApplication.run(RendezvousApplication.class, args);
     }
 
 }
