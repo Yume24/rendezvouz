d9b954e303d0f63343d86fa7ed0055a05d1ab801
Antoni Jaros <antonijaros04@gmail.com>
2026-08-26 20:55:31 +0200

    feat: add missing mapper methods for `AnonymousUser` and `RegisteredUser`

--- src/main/java/com/yume24/rendezvous/user/UserMapper.java
+++ src/main/java/com/yume24/rendezvous/user/UserMapper.java
@@ -8,7 +8,10 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 interface UserMapper {
     UserDTO toDto(AnonymousUser anonymousUser);
+
     UserDTO toDto(RegisteredUser user);
+
     User toEntity(AnonymousUser anonymousUser);
+
     User toEntity(RegisteredUser registeredUser);
 }
