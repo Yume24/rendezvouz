2255b2c788d88a544a03b9143db0e6c1ee6eb2c8
Antoni Jaros <antonijaros04@gmail.com>
2026-08-31 21:48:40 +0200

    revert: remove caching support from `RendezvousApplication`

--- src/main/java/com/yume24/rendezvous/RendezvousApplication.java
+++ src/main/java/com/yume24/rendezvous/RendezvousApplication.java
@@ -2,10 +2,8 @@ package com.yume24.rendezvous;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class RendezvousApplication {
 
     static void main(String[] args) {
