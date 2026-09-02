479d82f99054589a1aea52275a3f76db6f9c27e8
Antoni Jaros <antonijaros04@gmail.com>
2026-09-02 22:13:44 +0200

    feat: add `Customizer<AuthorizeExchangeSpec>` implementation in `AbstractWebsocketHandler` to permit all on dynamic WebSocket paths

--- src/main/java/com/yume24/rendezvous/websocket/handler/AbstractWebsocketHandler.java
+++ src/main/java/com/yume24/rendezvous/websocket/handler/AbstractWebsocketHandler.java
@@ -1,14 +1,21 @@
 package com.yume24.rendezvous.websocket.handler;
 
 import lombok.Getter;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity.AuthorizeExchangeSpec;
 import org.springframework.web.reactive.socket.WebSocketHandler;
 
 @Getter
-public abstract class AbstractWebsocketHandler implements WebSocketHandler {
+public abstract class AbstractWebsocketHandler implements WebSocketHandler, Customizer<AuthorizeExchangeSpec> {
     private static final String HANDLER_PATH = "/ws";
     private final String path;
 
     public AbstractWebsocketHandler(String path) {
         this.path = HANDLER_PATH + path;
+    }
+
+    @Override
+    public void customize(AuthorizeExchangeSpec authorizeExchangeSpec) {
+        authorizeExchangeSpec.pathMatchers(path).permitAll();
     }
 }
