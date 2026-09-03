afba52d992ae618dcbe375fe80ecf780596778b5
Antoni Jaros <antonijaros04@gmail.com>
2026-09-03 22:46:55 +0200

    feat: annotate `TicketService` with `@Service` and `@RequiredArgsConstructor` for DI support

--- src/main/java/com/yume24/rendezvous/websocket/service/TicketService.java
+++ src/main/java/com/yume24/rendezvous/websocket/service/TicketService.java
@@ -1,10 +1,14 @@
 package com.yume24.rendezvous.websocket.service;
 
 import com.yume24.rendezvous.websocket.dto.TicketDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
+@Service
+@RequiredArgsConstructor
 public class TicketService {
     public Mono<TicketDTO> createTicket(UUID userId) {
         return Mono.just(new TicketDTO(""));
