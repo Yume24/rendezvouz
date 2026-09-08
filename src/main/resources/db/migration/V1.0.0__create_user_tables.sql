09de18a00555e9b19b50ec822f1998713065ac12
Antoni Jaros <antonijaros04@gmail.com>
2026-09-08 19:46:11 +0200

    refactor: remove R2DBC configuration, custom converters, and switch `user_type` to `varchar` in schema

--- src/main/resources/db/migration/V1.0.0__create_user_tables.sql
+++ src/main/resources/db/migration/V1.0.0__create_user_tables.sql
@@ -1,13 +1,13 @@
-create type user_type as enum ('registered', 'anonymous');
+-- create type user_type as enum ('registered', 'anonymous');
 
 create table users (
     id uuid primary key default uuidv7(),
     username varchar(255) not null,
     password char(60),
-    type user_type not null,
+    type varchar(63) not null,
     created_at timestamptz not null default now(),
-    constraint chk_anonymous_user_no_pswd check (type != 'anonymous' or password is not null),
-    constraint chk_registered_user_has_pswd check(type != 'registered' or password is null)
+    constraint chk_anonymous_user_no_pswd check (type != 'anonymous' or password is null),
+    constraint chk_registered_user_has_pswd check(type != 'registered' or password is not null)
 );
 
 create unique index on users(username) where type = 'registered';
